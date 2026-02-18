f3577e4a62ca24e9fabc092ef4fcaef16495dee0
LeoKliemann <lgkliemann05@gmail.com>
2026-02-18 18:15:32 -0500

    Added pseudocode

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
+//class PartyAffiliation
+//	main()
+//		String party
+//		output “Please select your party [D, R, I]”
+//		input party
+//		if party == D then
+//			output “You get a Democrat Donkey!”
+//		else if party == R then
+//			output “You get a Republican Elephant!”
+//		else if party == I then
+//			output “You get an Independent Man!”
+//		else
+//			output “You have not entered a proper value:” + party
+//		endIf
+//	return
+//endClass
+
 import java.util.Scanner;
 public class Main{
     public static void main (String[] args)
     {
-        
+
     }
 }
